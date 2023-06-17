b272ed35d958b1da8a40c4060e2e093b24818109
ThinusvanStaden94 <109592693+ThinusvanStaden94@users.noreply.github.com>
2023-06-17 19:43:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript code for the website
+
+// Function to handle the "Surprise Dad" button click event
+function surpriseDad() {
+    alert("Surprise! Happy Father's Day, Dad!");
+  }
+  
+  // Add event listener to the "Surprise Dad" button
+  var surpriseBtn = document.getElementById("surpriseBtn");
+  surpriseBtn.addEventListener("click", surpriseDad);
